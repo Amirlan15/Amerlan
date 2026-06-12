de0e1caa4e5361dd47e62c810cff3b79a71c9b29
Amirlan <your_email@example.com>
2026-06-12 17:38:41 +0500

    final project

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let sdf = document.querySelector("#sdf");
 sdf.addEventListener('click', () => {
     stop.textContent = "Зачем нажал!?";
 });
-    stop.style.color = "#0066ff"
+    stop.style.color = "#3138389c"
     stop.style.fontWeight = "700"
 
     sdf.addEventListener("click", function() {
@@ -18,3 +18,14 @@ sdf.addEventListener('click', () => {
         stop.style.color = "#e53935";
         stop.style.fontWeight = "700";
     });
+let form = document.querySelector("#from");
+let formMessage = document.querySelector("#formMessage");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    formMessage.textContent="Спасибо! Сообщение отправлено.";
+    formMessage.style.color="green";
+
+    form.reset();
+})
